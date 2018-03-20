0ac6c08d9cdbf97726daab78e5aaf2ff3d81e939
Kay Mok <kaymok3@gmail.com>
2018-03-20 18:42:18 -0400

    Add divs for each product

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 // AJAX should append all src (images) to div#container, each in a div with class of .item
 // as well as append title (product name) underneath the images
+
+$.ajax({
+  url: "https://www.redbullshopus.com/products.json",
+  success: function(data) {
+    let products = data.products;
+    $(products).each(function(i) {
+      // console.log(products[i]);
+      $('#container').append($('<div></div>').addClass('item'));
+    })
+    let items = $('.item');
+    $(items).each(function(i) {
+      console.log(products[i].title);
+    })
+  }
+})
